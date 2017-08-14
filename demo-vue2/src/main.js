5645a65ad367a2eafabe9f46d8e6bb88390ed58e
isMyShell <1037822795@qq.com>
2017-08-14 13:10:59 +0800

    vConsole 移动端输出log的插件

--- demo-vue2/src/main.js
+++ demo-vue2/src/main.js
@@ -4,10 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
-
+import vConsole from 'vconsole';
 import 'animate.css'
 
-
+Vue.use(vConsole)
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
